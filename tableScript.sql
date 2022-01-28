0a4167047bf94c8a30dc2a1146397c8cbaac0355
Your Name <you@example.com>
2022-01-28 14:02:59 +0100

    updated readme

--- tableScript.sql
+++ tableScript.sql
@@ -1,14 +1,74 @@
-CREATE TABLE Services (
-    id                 INTEGER      PRIMARY KEY,
-    serviceDefinition  CHAR (50),
-    SystemName         TEXT         NOT NULL,
-    metaData           CHAR (45)    DEFAULT 0,
-    Port               INTEGER (10),
-    authenticationInfo CHAR (50),
-    serviceURI         CHAR (100),
-    endOfValidity      CHAR (50),
-    secure             CHAR (50),
-    address            CHAR (100),
-    version            CHAR (50),
-    interfaces         CHAR (100) 
+--
+-- File generated with SQLiteStudio v3.3.3 on fre jan 28 13:48:11 2022
+--
+-- Text encoding used: System
+--
+PRAGMA foreign_keys = off;
+BEGIN TRANSACTION;
+
+-- Table: Interfaces
+CREATE TABLE Interfaces (
+    idInterface   INTEGER,
+    serviceID     INTEGER,
+    interfaceName CHAR,
+    createdAt     CHAR,
+    updatedAt     CHAR,
+    PRIMARY KEY (
+        idInterface
+    )
+    AUTOINCREMENT,
+    FOREIGN KEY (
+        serviceID
+    )
+    REFERENCES Services (id) ON DELETE CASCADE
 );
+
+
+-- Table: MetaData
+CREATE TABLE MetaData (
+    idMetaData INTEGER,
+    serviceID  INTEGER,
+    metaData   CHAR,
+    PRIMARY KEY (
+        idMetaData
+    )
+    AUTOINCREMENT,
+    FOREIGN KEY (
+        serviceID
+    )
+    REFERENCES Services (id) ON DELETE CASCADE
+);
+
+
+-- Table: Services
+CREATE TABLE Services (
+    id                         INTEGER NOT NULL,
+    serviceDefinition          CHAR    NOT NULL,
+    serviceDefinitionCreatedAt CHAR    NOT NULL
+                                       DEFAULT 'defaultValue',
+    serviceDefinitionUpdatedAt CHAR    NOT NULL
+                                       DEFAULT 'defaultValue',
+    systemName                 CHAR    NOT NULL,
+    address                    CHAR    NOT NULL,
+    port                       INTEGER NOT NULL,
+    authenticationInfo         CHAR    NOT NULL,
+    providerCreatedAt          CHAR    NOT NULL
+                                       DEFAULT 'defaultValue',
+    providerUpdatedAt          CHAR    NOT NULL
+                                       DEFAULT 'defaultValue',
+    serviceURI                 CHAR    NOT NULL,
+    endOfValidity              CHAR    NOT NULL,
+    secure                     CHAR    NOT NULL,
+    version                    CHAR    NOT NULL,
+    createdAt                  CHAR    NOT NULL
+                                       DEFAULT 'defaultValue',
+    updatedAt                  CHAR    NOT NULL
+                                       DEFAULT 'defaultValue',
+    PRIMARY KEY (
+        id
+    )
+);
+
+
+COMMIT TRANSACTION;
+PRAGMA foreign_keys = on;
